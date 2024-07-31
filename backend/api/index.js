81e96c2bb3c268df4f82367e416ed4096ce03fbc
Emoumo-coder <sandaumaru207@gmail.com>
2024-07-31 20:04:39 +0100

    set up vercel for backend

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('./config/db');
-const portfolioRoutes = require('./routes/portfolioRoutes');
+const connectDB = require('../config/db');
+const portfolioRoutes = require('../routes/portfolioRoutes');
 const bodyParser = require('body-parser');
 
 dotenv.config();
@@ -26,3 +26,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
